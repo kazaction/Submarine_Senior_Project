75853c6d9cc7e0f0a9aea620dd4eb32166d50268
kazaction <ps4psnkikos@gmail.com>
2026-03-19 17:04:49 +0200

    switched from onnx manual setup to ultralytics yolo (peak)

--- main.py
+++ main.py
@@ -1,44 +1,23 @@
-import onnxruntime
-import onnx
+from ultralytics import YOLO
 import cv2
-import numpy
 
-filename = "./runs/detect/train14/weights/best.onnx"
-onnx_model = onnx.load(filename)
-cam = cv2.VideoCapture(0)
-onnxruntime.InferenceSession(filename)
+model = YOLO("./runs/detect/train14/weights/best.onnx")
+
+cap = cv2.VideoCapture(0)
 
 while True:
-    #print("read")
-    ret, frame = cam.read()
-    # Display the frame with results
-    cv2.imshow('Submarine Camera Feed', frame)
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-    #q button exits camera
+    results = model(frame, imgsz=640, conf=0.35)
+
+    annotated = results[0].plot()
+
+    cv2.imshow("Submarine Camera Feed", annotated)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    #destroy resources allocated
-    if not ret:
-        cam.release()
-        cv2.destroyAllWindows()
-        break
-
-
-
-#run(filename)
-
-    input_size = (640, 640)
-    frame_resized = cv2.resize(frame, input_size)
-    input_tensor = numpy.array(frame_resized).astype(numpy.float32)
-    input_tensor = numpy.expand_dims(input_tensor, axis=0)
-
-
-    # Run inference
-    #outputs = ort_session.run(None, {ort_session.get_inputs()[0].name: input_tensor})
-
-
-# Process the outputs as needed
-
-
-
+cap.release()
+cv2.destroyAllWindows()
